b0e84d38771c9ba47e064de9922ed4f71bc09996
sevdaerdem <123573889+sevdaerdem@users.noreply.github.com>
2023-04-15 15:11:45 +0300

    new

---
+++ src/test/resources/MyStepdefs.java
@@ -0,0 +1,5 @@
+public class MyStepdefs {
+    @io.cucumber.java.en.Then("^Delete Subject Categories for setting different education$")
+    public void deleteSubjectCategoriesForSettingDifferentEducation() {
+    }
+}
